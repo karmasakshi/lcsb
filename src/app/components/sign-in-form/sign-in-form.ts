f7b08c294452679c961753d2880f052149b045b1
karmasakshi <karmasakshi@users.noreply.github.com>
2025-07-22 16:30:42 +0400

    chore(general): enable onpush change detection

--- src/app/components/sign-in-form/sign-in-form.ts
+++ src/app/components/sign-in-form/sign-in-form.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'lcsb-sign-in-form',
   imports: [],
   templateUrl: './sign-in-form.html',
