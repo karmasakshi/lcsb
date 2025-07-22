37bbc808572762f8952c2b65eb1d58a591ac11ec
karmasakshi <karmasakshi@users.noreply.github.com>
2025-07-22 16:44:37 +0400

    chore(general): add navigation

--- src/app/components/app/app.ts
+++ src/app/components/app/app.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navigation } from "../navigation/navigation";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'lcsb-app',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navigation],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/components/navigation/navigation.ts
+++ src/app/components/navigation/navigation.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'lcsb-navigation',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatToolbarModule, RouterLink],
   templateUrl: './navigation.html',
   styleUrl: './navigation.scss'
 })
