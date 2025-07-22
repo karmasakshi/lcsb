45553d35eb6dd943c85c3cd35f4155abec9dfae4
karmasakshi <karmasakshi@users.noreply.github.com>
2025-07-22 16:15:22 +0400

    chore(general): generate bp page component

---
+++ src/app/components/bp-page/bp-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BpPage } from './bp-page';
+
+describe('BpPage', () => {
+  let component: BpPage;
+  let fixture: ComponentFixture<BpPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BpPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BpPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/bp-page/bp-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lcsb-bp-page',
+  imports: [],
+  templateUrl: './bp-page.html',
+  styleUrl: './bp-page.scss'
+})
+export class BpPage {
+
+}
