7d0d79a8d447c8c3fb2c4ee48264ab994c47e505
karmasakshi <karmasakshi@users.noreply.github.com>
2025-07-22 16:18:08 +0400

    chore(general): generate bp chart form component

---
+++ src/app/components/bp-chart-form/bp-chart-form.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BpChartForm } from './bp-chart-form';
+
+describe('BpChartForm', () => {
+  let component: BpChartForm;
+  let fixture: ComponentFixture<BpChartForm>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BpChartForm]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BpChartForm);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/bp-chart-form/bp-chart-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lcsb-bp-chart-form',
+  imports: [],
+  templateUrl: './bp-chart-form.html',
+  styleUrl: './bp-chart-form.scss'
+})
+export class BpChartForm {
+
+}
