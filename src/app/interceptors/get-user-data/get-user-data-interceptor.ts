4d17b6ccade264a359397a8e3ce8b347cf9f706b
karmasakshi <karmasakshi@users.noreply.github.com>
2025-07-22 17:34:55 +0400

    chore(general): add interceptors

--- src/app/interceptors/get-user-data/get-user-data-interceptor.ts
+++ src/app/interceptors/get-user-data/get-user-data-interceptor.ts
@@ -1,5 +1,16 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpHeaders, HttpInterceptorFn, HttpResponse } from '@angular/common/http';
+import { of } from 'rxjs';
 
 export const getUserDataInterceptor: HttpInterceptorFn = (req, next) => {
+  const userDataApiRegex = /\/user\/[^\/]+\/data$/;
+
+  if (req.method === 'GET' && userDataApiRegex.test(req.url)) {
+    return of(new HttpResponse({
+      body: { blood_pressures: [] },
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      status: 200
+    }));
+  }
+
   return next(req);
 };
--- src/app/interceptors/post-login/post-login-interceptor.ts
+++ src/app/interceptors/post-login/post-login-interceptor.ts
@@ -1,5 +1,14 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpHeaders, HttpInterceptorFn, HttpResponse } from '@angular/common/http';
+import { of } from 'rxjs';
 
 export const postLoginInterceptor: HttpInterceptorFn = (req, next) => {
+  if (req.method === 'POST' && req.url.endsWith('/login')) {
+    return of(new HttpResponse({
+      body: { token: 'token' } ,
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      status: 200
+    }));
+  }
+
   return next(req);
 };
