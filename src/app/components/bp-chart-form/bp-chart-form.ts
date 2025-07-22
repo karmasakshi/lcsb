4686e65c897cf1bb5384b8094bdfd42c198db1e1
karmasakshi <karmasakshi@users.noreply.github.com>
2025-07-22 16:19:35 +0400

    chore(general): enable onpush change detection

--- src/app/components/bp-chart-form/bp-chart-form.ts
+++ src/app/components/bp-chart-form/bp-chart-form.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'lcsb-bp-chart-form',
   imports: [],
   templateUrl: './bp-chart-form.html',
--- src/app/components/bp-page/bp-page.ts
+++ src/app/components/bp-page/bp-page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'lcsb-bp-page',
   imports: [],
   templateUrl: './bp-page.html',
