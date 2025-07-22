4d13118b1f22b79cc1461b02ebe97536af5306e9
karmasakshi <karmasakshi@users.noreply.github.com>
2025-07-22 16:06:38 +0400

    chore(general): generate app component

---
+++ src/app/components/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lcsb-app',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+
+}
