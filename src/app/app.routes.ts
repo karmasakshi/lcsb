7074b9b31a6fdf6ed1db4abc3f017394a5196572
karmasakshi <karmasakshi@users.noreply.github.com>
2025-07-22 16:28:06 +0400

    chore(general): update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BpPage } from './components/bp-page/bp-page';
+import { HomePage } from './components/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 'bp', component: BpPage },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
