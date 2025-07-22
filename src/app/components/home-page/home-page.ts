9dc73058d5b8a5071c6069fe7364b44eac27828e
karmasakshi <karmasakshi@users.noreply.github.com>
2025-07-22 16:10:04 +0400

    chore(general): generate home page component

---
+++ src/app/components/home-page/home-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lcsb-home-page',
+  imports: [],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.scss'
+})
+export class HomePage {
+
+}
